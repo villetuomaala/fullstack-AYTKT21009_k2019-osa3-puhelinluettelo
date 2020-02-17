9fd0879bb3a3e1975925cd8193d2934a0746c691
Ville Tuomaala <ville.tuomaala@almamedia.fi>
2020-02-17 23:17:58 +0200

    3.8*: puhelinluettelon backend step8

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
+morgan.token('data', (req, res) => JSON.stringify(req.body))
 
 app.use(express.json())
-app.use(morgan('tiny')) 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data')) 
 
 let data = [
   {
