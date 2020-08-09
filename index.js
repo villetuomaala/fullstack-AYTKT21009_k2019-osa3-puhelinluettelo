267b20acebfcd6b23501a167185af5b082274cb8
Ville Tuomaala <ville.tuomaala@almamedia.fi>
2020-08-10 00:01:23 +0300

    Add cors dependency

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 morgan.token('data', (req, res) => JSON.stringify(req.body))
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data')) 
+
+app.use(cors())
 
 let data = [
   {
