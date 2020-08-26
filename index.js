3cbc69e642adb605aa030b7303872cf791229848
Ville Tuomaala <ville.tuomaala@almamedia.fi>
2020-08-26 22:00:02 +0300

    3.15: puhelinluettelo ja tietokanta, step3

--- index.js
+++ index.js
@@ -61,9 +61,8 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  data = data.filter(d => d.id != id)
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => response.status(204).end())
 })
 
 app.post('/api/persons', (request, response) => {
